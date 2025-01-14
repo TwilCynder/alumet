759bd3e7c988ccf1948a8938b208e42fdf587d7f
TwilCynder <twilcynder@gmail.com>
2025-01-14 18:20:21 +0100

    mise en place basique plugin

--- app-agent/src/bin/local.rs
+++ app-agent/src/bin/local.rs
@@ -15,11 +15,8 @@ use serde::{Deserialize, Serialize};
 fn main() {
     // Specify here all the plugins that will be included in the agent during compilation.
     let plugins = static_plugins![
-        plugin_rapl::RaplPlugin,
-        plugin_perf::PerfPlugin,
-        plugin_procfs::ProcfsPlugin,
         plugin_csv::CsvPlugin,
-        plugin_socket_control::SocketControlPlugin,
+        plugin_test::ExamplePlugin
     ];
 
     init_logger();
