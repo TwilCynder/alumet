70c01a6cb871f5e56e551f25ba4706e7237ccf33
TwilCynder <twilcynder@gmail.com>
2025-02-10 16:52:45 +0100

    Minimal example of plugin calling mojitos functions

--- plugin-mojitos-c/src/lib.rs
+++ plugin-mojitos-c/src/lib.rs
@@ -1,4 +1,7 @@
 use alumet::plugin::{rust::AlumetPlugin, AlumetPluginStart, ConfigTable};
+use mojitos::clean;
+
+mod mojitos;
 
 pub struct MojitOSCPlugin;
 
@@ -21,10 +24,21 @@ impl AlumetPlugin for MojitOSCPlugin {
 
     fn start(&mut self, alumet: &mut AlumetPluginStart) -> anyhow::Result<()> {
         log::info!("Hello!");
+        unsafe {
+            let mut args = [b"-c\0".as_ptr(), std::ptr::null()];
+            
+            let nb = mojitos::init((args.as_mut_ptr())as *mut *mut i8);
+            let labels = mojitos::get_labels();
+
+        }
         Ok(())
     }
 
     fn stop(&mut self) -> anyhow::Result<()> {
+        unsafe {
+            clean();
+        }
+
         log::info!("Bye!");
         Ok(())
     }
