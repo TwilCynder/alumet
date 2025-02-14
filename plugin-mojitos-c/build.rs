7ff9c49d9f8caf476e326e711c200880f2861bee
TwilCynder <twilcynder@gmail.com>
2025-02-15 00:11:00 +0100

    link to installed mojitos lib directly

--- plugin-mojitos-c/build.rs
+++ plugin-mojitos-c/build.rs
@@ -1,9 +1,15 @@
+const HEADER_DIR: &str = "/usr/local/include/";
+const LIB_DIR: &str = "/usr/local/lib/";
+
 fn main(){
     println!("cargo::rerun-if-changed=src/c/");
+    println!("cargo:rustc-link-lib=mojitos");
+
 
     bindgen::builder()
-        .header("src/c/libmojitos.h")
-        .blocklist_file("/usr/.*")
+        .header(HEADER_DIR.to_owned() + "/mojitos.h")
+        .blocklist_file("/usr/lib/.*")
+        .blocklist_file("/usr/include.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate().expect("Coulnd't generate bindings").write_to_file("src/mojitos.rs").expect("Couldn't write bindings to file");
 }
